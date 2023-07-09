fcf1378fa6d214a8e522139f98663a1630a9df1c
sazzad4677 <sazzad35-1898@diu.edu.bd>
2023-07-09 23:02:15 +0600

    Check setup

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request } from "express";
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: any) => {
   res.send("Hello World!");
 });
 
